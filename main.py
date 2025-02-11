d07f7b30c644ae7f3e4403e2f40bc98ed4c55d4b
vaidehiakbari <akbari4846@mydusd.org>
2025-02-11 13:54:40 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+print(time.gmtime(0))
+"""
+from datetime import datetime
+
+def time_left(current_time_str, end_time_str):
+   
+    time_format = "%H:%M"
+
+    current_time = datetime.strptime(current_time_str, time_format)
+    end_time = datetime.strptime(end_time_str, time_format)
+
+    #difference
+    time_difference = end_time - current_time
+    minutes_left = time_difference.total_seconds() / 60
+
+ 
+    if minutes_left > 0:
+        print(f"There are {int(minutes_left)} minutes left until class ends.")
+    else:
+        print("Class has already ended!")
+
+
+current_time = input("Enter the current time (Hour:Minute): ")
+end_time = input("Enter the class end time (Hour:Minute): ")
+time_left(current_time, end_time)
+"""
